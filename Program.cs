9ade6cb14ff49da77433410d91788176968ec5b5
Anna Khit <annakhit02@mail.ru>
2024-11-30 15:28:38 +0300

    Implemented String Parser

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace KASDLab25
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string[] rows = File.ReadAllLines("../../Data/input.txt");
+
+            MyHashSet<StringLine> hashSet = new MyHashSet<StringLine>();
+
+            foreach (string row in rows) hashSet.Add(new StringLine(row));
+
+            foreach (StringLine line in hashSet.ToArray()) Console.WriteLine("Value: {0}", line.value);
+
+            Console.ReadKey();
         }
     }
 }
